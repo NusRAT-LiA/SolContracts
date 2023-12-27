7360a74468b9a43c3496102a15062158581cd682
NusRAT-Lia <nusratlia424@gmail.com>
2023-12-28 00:14:31 +0600

    Client-side connected to testnet

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+
+//const { REACT_APP_PROJECT_ID } = process.env;
+const alchemyUrl = `https://eth-goerli.g.alchemy.com/v2/${process.env.REACT_APP_PROJECT_ID}`;
+const web3 = new Web3(alchemyUrl);
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    web3.eth.net.isListening()
+      .then(() => {
+        setIsConnected(true);
+      })
+      .catch(() => {
+        setIsConnected(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {isConnected ? 'Connected to Alchemy' : 'Not connected to Alchemy'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isConnected && (
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        )}
       </header>
     </div>
   );
